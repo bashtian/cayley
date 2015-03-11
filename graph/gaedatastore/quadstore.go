a097fb590d7bc0505a87732ee562283a84ba02dd
bashtian <bashtian86@gmail.com>
2015-03-11 12:04:26 +0100

    Add caching

--- graph/gaedatastore/quadstore.go
+++ graph/gaedatastore/quadstore.go
@@ -24,14 +24,18 @@ import (
 	"math"
 	"net/http"
 	"sync"
-
-	"appengine"
-	"appengine/datastore"
-	"github.com/barakmich/glog"
+	"time"
 
 	"github.com/google/cayley/graph"
 	"github.com/google/cayley/graph/iterator"
 	"github.com/google/cayley/quad"
+
+	"github.com/barakmich/glog"
+	"github.com/mjibson/goon"
+
+	"appengine"
+	"appengine/datastore"
+	"appengine/memcache"
 )
 
 const (
@@ -47,13 +51,28 @@ var (
 		New: func() interface{} { return sha1.New() },
 	}
 	hashSize = sha1.Size
+	// instance cache for datastore query results
+	cache     = map[string]Cache{}
+	cacheTime = time.Now()
 )
 
 type QuadStore struct {
-	context appengine.Context
+	hashSize   int
+	makeHasher func() hash.Hash
+	context    appengine.Context
+	db         *goon.Goon
+	//cache      map[string]Cache
+}
+
+type Cache struct {
+	buffer []string
+	last   string
+	done   bool
 }
 
 type MetadataEntry struct {
+	Id        string `datastore:"-" goon:"id"`
+	_kind     string `goon:"kind,metadata"`
 	NodeCount int64
 	QuadCount int64
 }
@@ -64,6 +83,8 @@ type Token struct {
 }
 
 type QuadEntry struct {
+	Id        string `datastore:"-" goon:"id"`
+	Kind      string `datastore:"-" goon:"kind,quad"`
 	Hash      string
 	Added     []string `datastore:",noindex"`
 	Deleted   []string `datastore:",noindex"`
@@ -74,11 +95,15 @@ type QuadEntry struct {
 }
 
 type NodeEntry struct {
-	Name string
-	Size int64
+	Id    string `datastore:"-" goon:"id"`
+	_kind string `goon:"kind,node"`
+	Name  string
+	Size  int64
 }
 
 type LogEntry struct {
+	Id        string `datastore:"-" goon:"id"`
+	_kind     string `goon:"kind,logentry"`
 	LogID     string
 	Action    string
 	Key       string
@@ -87,6 +112,35 @@ type LogEntry struct {
 
 func init() {
 	graph.RegisterQuadStore("gaedatastore", true, newQuadStore, initQuadStore, newQuadStoreForRequest)
+}
+
+func (qs *QuadStore) resetCache() {
+	cache = make(map[string]Cache)
+	cacheTime = time.Now()
+	t, _ := cacheTime.MarshalBinary()
+	item := &memcache.Item{
+		Key:   "last_cache_reset",
+		Value: t,
+	}
+	// Add the item to the memcache, if the key does not already exist
+	if err := memcache.Add(qs.db.Context, item); err == memcache.ErrNotStored {
+		qs.db.Context.Infof("item with key %q already exists", item.Key)
+	} else if err != nil {
+		qs.db.Context.Errorf("error adding item: %v", err)
+	}
+}
+
+func (qs *QuadStore) checkCache() {
+	// Get the item from the memcache
+	if item, err := memcache.Get(qs.db.Context, "last_cache_reset"); err != nil {
+		qs.resetCache()
+	} else {
+		t := time.Time{}
+		t.UnmarshalBinary(item.Value)
+		if cacheTime.Before(time.Now()) {
+			qs.resetCache()
+		}
+	}
 }
 
 func initQuadStore(_ string, _ graph.Options) error {
@@ -106,6 +160,7 @@ func newQuadStoreForRequest(qs graph.QuadStore, options graph.Options) (graph.Qu
 	}
 	t := newQs.(*QuadStore)
 	t.context, err = getContext(options)
+	t.db = goon.FromContext(t.context)
 	return newQs, err
 }
 
@@ -135,9 +190,9 @@ func (qs *QuadStore) createKeyFromToken(t *Token) *datastore.Key {
 }
 
 func (qs *QuadStore) checkValid(k *datastore.Key) (bool, error) {
-	var q quad.Quad
-	err := datastore.Get(qs.context, k, &q)
-	if err == datastore.ErrNoSuchEntity {
+	q := QuadEntry{Id: k.StringID(), Kind: k.Kind()}
+	err := qs.db.Get(&q)
+	if err == datastore.ErrNoSuchEntity || len(q.Added) <= len(q.Deleted) {
 		return false, nil
 	}
 	if _, ok := err.(*datastore.ErrFieldMismatch); ok {
@@ -203,6 +258,7 @@ func (qs *QuadStore) ApplyDeltas(in []graph.Delta, ignoreOpts graph.IgnoreOpts) 
 	if len(toKeep) == 0 {
 		return nil
 	}
+	qs.resetCache()
 	err := qs.updateLog(toKeep)
 	if err != nil {
 		glog.Errorf("Updating log failed %v", err)
@@ -251,19 +307,22 @@ func (qs *QuadStore) updateNodes(in []graph.Delta) (int64, error) {
 		nodesAdded += countDelta
 	}
 	// Create keys and new nodes
-	keys := make([]*datastore.Key, 0, len(nodeDeltas))
 	tempNodes := make([]NodeEntry, 0, len(nodeDeltas))
 	for k, v := range nodeDeltas {
-		keys = append(keys, qs.createKeyForNode(k))
-		tempNodes = append(tempNodes, NodeEntry{k, v})
+		key := qs.createKeyForNode(k)
+		n := NodeEntry{Id: key.StringID(), _kind: key.Kind(), Name: k, Size: v}
+		tempNodes = append(tempNodes, n)
 	}
 	// In accordance with the appengine datastore spec, cross group transactions
 	// like these can only be done in batches of 5
 	for i := 0; i < len(nodeDeltas); i += 5 {
 		j := int(math.Min(float64(len(nodeDeltas)-i), 5))
 		foundNodes := make([]NodeEntry, j)
-		err := datastore.RunInTransaction(qs.context, func(c appengine.Context) error {
-			err := datastore.GetMulti(c, keys[i:i+j], foundNodes)
+		for k := range foundNodes {
+			foundNodes[k].Id = tempNodes[i+k].Id
+		}
+		err := qs.db.RunInTransaction(func(tg *goon.Goon) error {
+			err := tg.GetMulti(foundNodes)
 			// Sift through for errors
 			if me, ok := err.(appengine.MultiError); ok {
 				for _, merr := range me {
@@ -279,7 +338,7 @@ func (qs *QuadStore) updateNodes(in []graph.Delta) (int64, error) {
 					tempNodes[i+k].Size += foundNodes[k].Size
 				}
 			}
-			_, err = datastore.PutMulti(c, keys[i:i+j], tempNodes[i:i+j])
+			_, err = tg.PutMulti(tempNodes[i : i+j])
 			return err
 		}, &datastore.TransactionOptions{XG: true})
 		if err != nil {
@@ -292,37 +351,39 @@ func (qs *QuadStore) updateNodes(in []graph.Delta) (int64, error) {
 }
 
 func (qs *QuadStore) updateQuads(in []graph.Delta) (int64, error) {
-	keys := make([]*datastore.Key, 0, len(in))
-	for _, d := range in {
-		keys = append(keys, qs.createKeyForQuad(d.Quad))
+	foundQuads := make([]QuadEntry, len(in))
+	for i, d := range in {
+		key := qs.createKeyForQuad(d.Quad)
+		foundQuads[i].Id = key.StringID()
+		foundQuads[i].Kind = key.Kind()
 	}
 	var quadCount int64
 	for i := 0; i < len(in); i += 5 {
 		// Find the closest batch of 5
 		j := int(math.Min(float64(len(in)-i), 5))
-		err := datastore.RunInTransaction(qs.context, func(c appengine.Context) error {
-			foundQuads := make([]QuadEntry, j)
+		err := qs.db.RunInTransaction(func(tg *goon.Goon) error {
 			// We don't process errors from GetMulti as they don't mean anything,
 			// we've handled existing quad conflicts above and we overwrite everything again anyways
-			datastore.GetMulti(c, keys, foundQuads)
-			for k, _ := range foundQuads {
+
+			tg.GetMulti(foundQuads[i : i+j])
+			for k, _ := range foundQuads[i : i+j] {
 				x := i + k
-				foundQuads[k].Hash = keys[x].StringID()
-				foundQuads[k].Subject = in[x].Quad.Subject
-				foundQuads[k].Predicate = in[x].Quad.Predicate
-				foundQuads[k].Object = in[x].Quad.Object
-				foundQuads[k].Label = in[x].Quad.Label
+				foundQuads[x].Hash = foundQuads[x].Id
+				foundQuads[x].Subject = in[x].Quad.Subject
+				foundQuads[x].Predicate = in[x].Quad.Predicate
+				foundQuads[x].Object = in[x].Quad.Object
+				foundQuads[x].Label = in[x].Quad.Label
 
 				// If the quad exists the Added[] will be non-empty
 				if in[x].Action == graph.Add {
-					foundQuads[k].Added = append(foundQuads[k].Added, in[x].ID.String())
+					foundQuads[x].Added = append(foundQuads[x].Added, in[x].ID.String())
 					quadCount += 1
 				} else {
-					foundQuads[k].Deleted = append(foundQuads[k].Deleted, in[x].ID.String())
+					foundQuads[x].Deleted = append(foundQuads[x].Deleted, in[x].ID.String())
 					quadCount -= 1
 				}
 			}
-			_, err := datastore.PutMulti(c, keys[i:i+j], foundQuads)
+			_, err := tg.PutMulti(foundQuads[i : i+j])
 			return err
 		}, &datastore.TransactionOptions{XG: true})
 		if err != nil {
@@ -334,16 +395,16 @@ func (qs *QuadStore) updateQuads(in []graph.Delta) (int64, error) {
 
 func (qs *QuadStore) updateMetadata(quadsAdded int64, nodesAdded int64) error {
 	key := qs.createKeyForMetadata()
-	foundMetadata := new(MetadataEntry)
-	err := datastore.RunInTransaction(qs.context, func(c appengine.Context) error {
-		err := datastore.Get(c, key, foundMetadata)
+	foundMetadata := &MetadataEntry{Id: key.StringID(), _kind: key.Kind()}
+	err := qs.db.RunInTransaction(func(tg *goon.Goon) error {
+		err := tg.Get(foundMetadata)
 		if err != nil && err != datastore.ErrNoSuchEntity {
 			glog.Errorf("Error: %v", err)
 			return err
 		}
 		foundMetadata.QuadCount += quadsAdded
 		foundMetadata.NodeCount += nodesAdded
-		_, err = datastore.Put(c, key, foundMetadata)
+		_, err = tg.Put(foundMetadata)
 		if err != nil {
 			glog.Errorf("Error: %v", err)
 		}
@@ -361,7 +422,6 @@ func (qs *QuadStore) updateLog(in []graph.Delta) error {
 		return errors.New("Nothing to log")
 	}
 	logEntries := make([]LogEntry, 0, len(in))
-	logKeys := make([]*datastore.Key, 0, len(in))
 	for _, d := range in {
 		var action string
 		if d.Action == graph.Add {
@@ -371,16 +431,15 @@ func (qs *QuadStore) updateLog(in []graph.Delta) error {
 		}
 
 		entry := LogEntry{
+			Id:        d.ID.String(),
 			LogID:     d.ID.String(),
 			Action:    action,
 			Key:       qs.createKeyForQuad(d.Quad).String(),
 			Timestamp: d.Timestamp.UnixNano(),
 		}
 		logEntries = append(logEntries, entry)
-		logKeys = append(logKeys, qs.createKeyForLog(d.ID))
 	}
-
-	_, err := datastore.PutMulti(qs.context, logKeys, logEntries)
+	_, err := qs.db.PutMulti(logEntries)
 	if err != nil {
 		glog.Errorf("Error updating log: %v", err)
 	}
@@ -417,10 +476,8 @@ func (qs *QuadStore) NameOf(val graph.Value) string {
 		return ""
 	}
 
-	// TODO (panamafrancis) implement a cache
-
-	node := new(NodeEntry)
-	err := datastore.Get(qs.context, key, node)
+	node := NodeEntry{Id: key.StringID()}
+	err := qs.db.Get(&node)
 	if err != nil {
 		glog.Errorf("Error: %v", err)
 		return ""
@@ -441,8 +498,11 @@ func (qs *QuadStore) Quad(val graph.Value) quad.Quad {
 		return quad.Quad{}
 	}
 
-	q := new(QuadEntry)
-	err := datastore.Get(qs.context, key, q)
+	q := &QuadEntry{
+		Id:   key.StringID(),
+		Kind: key.Kind(),
+	}
+	err := qs.db.Get(q)
 	if err != nil {
 		// Red herring error : ErrFieldMismatch can happen when a quad exists but a field is empty
 		if _, ok := err.(*datastore.ErrFieldMismatch); !ok {
@@ -461,9 +521,14 @@ func (qs *QuadStore) Size() int64 {
 		glog.Error("Error fetching size, context is nil, graph not correctly initialised")
 		return 0
 	}
+	//qs.context.Infof("Size %v", qs)
+
 	key := qs.createKeyForMetadata()
+
 	foundMetadata := new(MetadataEntry)
-	err := datastore.Get(qs.context, key, foundMetadata)
+	foundMetadata.Id = key.StringID()
+	foundMetadata._kind = key.Kind()
+	err := qs.db.Get(foundMetadata)
 	if err != nil {
 		glog.Warningf("Error: %v", err)
 		return 0
@@ -476,9 +541,12 @@ func (qs *QuadStore) NodeSize() int64 {
 		glog.Error("Error fetching node size, context is nil, graph not correctly initialised")
 		return 0
 	}
+
 	key := qs.createKeyForMetadata()
 	foundMetadata := new(MetadataEntry)
-	err := datastore.Get(qs.context, key, foundMetadata)
+	foundMetadata.Id = key.StringID()
+	foundMetadata._kind = key.Kind()
+	err := qs.db.Get(foundMetadata)
 	if err != nil {
 		glog.Warningf("Error: %v", err)
 		return 0
@@ -494,7 +562,7 @@ func (qs *QuadStore) Horizon() graph.PrimaryKey {
 	// Query log for last entry...
 	q := datastore.NewQuery("logentry").Order("-Timestamp").Limit(1)
 	var logEntries []LogEntry
-	_, err := q.GetAll(qs.context, &logEntries)
+	_, err := qs.db.GetAll(q, &logEntries)
 	if err != nil || len(logEntries) == 0 {
 		// Error fetching horizon, probably graph is empty
 		return graph.NewUniqueKey("")
